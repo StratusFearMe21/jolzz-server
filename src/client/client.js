a0fa97b2775334b1cc8fbdf97bdf51e4b9d04d7a
Isaac Mills <rooster0055@protonmail.com>
2025-11-24 21:25:28 -0700

    Maybe don't publish the image on pull requetss

--- src/client/client.js
+++ src/client/client.js
@@ -1,7 +1,9 @@
-let serverIP = "0.0.0.0";
-let serverPort = 3333;
+import { parseArgs } from "util";
 
-const websocket = new WebSocket(`ws://${serverIP}:${serverPort}`);
+let serverIP = "b16adab1-b381-42ef-becb-c0d6cde93283-jolzz.tysonjenkins.dev"
+let serverPort = "443"
+
+const websocket = new WebSocket(`wss://${serverIP}:${serverPort}`);
 
 websocket.onopen = () => {
   console.info(`Opened WebSocket connection on ws://${serverIP}:${serverPort}`);
